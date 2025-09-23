6e6fcd5cdfea01a036b9d484f282089d0a69fadd
nitapb <nitapasaribu01@gmail.com>
2025-09-24 06:01:07 +0700

    perbaikan csrf

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -34,6 +34,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nita-pasaribu-footballshop.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://nita-pasaribu-footballshop.pbp.cs.ui.ac.id"
+]
 
 # Application definition
 
